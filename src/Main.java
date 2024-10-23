09c4790be1d51fe60e04e6945671b4fc8c00da08
ThomasTimmH <thha0006@stud.kea.dk>
2024-10-23 09:36:42 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Movie move = new Movie("Shawshank Redemption", "Frank Darabont", 1994, true, 144, "Drama");
+        Movie movie1 = new Movie(" The Shawshank Redemption", "Frank Darabont", 1994, true, 144, "Drama");
 Scanner scan = new Scanner(System.in);
+        System.out.println(movie1);
 
     }
 }
