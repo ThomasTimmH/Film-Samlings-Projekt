792952b5df01d86fee822d5e0c751db75eef910b
ThomasTimmH <thha0006@stud.kea.dk>
2024-10-23 09:12:59 +0200

    Commit

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,8 @@
 public class Movie {
+    private String title;
+    private String director;
+    private int yearCreated;
+    private boolean isInColor;
+    private int lengthInMinutes;
+    private String genre;
 }
