071b1dfdccf471d9c85c652e6c32a45e04620376
huma0001 <hussainomar5@gmail.com>
2024-10-23 09:52:51 +0200

    hoi

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         Movie move = new Movie("Shawshank Redemption", "Frank Darabont", 1994, true, 144, "Drama");
         Scanner scan = new Scanner(System.in);
 
+        MovieCollection movieCollection = new MovieCollection();
+
         System.out.println(move);
 
+        String userTitle = null;
+
+        System.out.println("Enter Title:");
+        userTitle = scan.nextLine();
+        System.out.println(userTitle);
     }
 }
