b653a3868f377539e79b50736b50b1ce56446460
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-10-23 09:14:01 +0200

    Dad

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,11 @@
 public class Movie {
+    private String title;
+    private String director;
+    private int yearCreated;
+    private boolean isInColor;
+    private int lengthInMinutes;
+    private String genre;
+
+
+
 }
