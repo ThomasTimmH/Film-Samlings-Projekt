f0ee7c21d4980b0f7825483184ab3e645bf096e7
huma0001 <hussainomar5@gmail.com>
2024-10-23 11:31:51 +0200

    User story 3 - Tilføjet muligheden for at se "movie overview"/film overblik og har gjort den mere struktureret - Fjernet true/false og lavet det om til ja/nej - tilføjet også muligheden for at kunne se movie listen i menu'en

--- src/MovieController.java
+++ src/MovieController.java
@@ -14,7 +14,8 @@ public class MovieController {
 
         while (running) {
             System.out.println("1. Create movie entry");
-            System.out.println("2. Exit");
+            System.out.println("2. See movie list");
+            System.out.println("3. Exit");
             int userResponse = scan.nextInt();
             switch (userResponse) {
                 case 1 -> {
@@ -31,12 +32,6 @@ public class MovieController {
                     System.out.println("Is the movie colored?");
                     scan.nextLine();
                     String userColor = scan.nextLine();
-                    /*
-                    if (userColor.equalsIgnoreCase("yes"){
-                        userColor = "yes";
-                    } else if(userColor.equalsIgnoreCase())
-
-                     */
 
                     System.out.println("Enter how long the movies is");
                     int movieLength = scan.nextInt();
@@ -50,10 +45,9 @@ public class MovieController {
                     Movie userMovie = new Movie(userTitle, userDirector, movieYear, userColor, movieLength, movieGenre);
 
                     movieCollection.addMovie(userMovie);
-
-                    System.out.println(movieCollection);
                 }
-                case 2 -> running = false;
+                case 2 -> System.out.println(movieCollection);
+                case 3 -> running = false;
             }
         }
     }
