4b649f299122a1c29c99566057e77bed5619a194
huma0001 <hussainomar5@gmail.com>
2024-10-24 13:30:36 +0200

    User story 3 - Tilføjet muligheden for at se "movie overview"/film overblik og har gjort den mere struktureret - Fjernet true/false og lavet det om til ja/nej - tilføjet også muligheden for at kunne se movie listen i menu'en

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -13,16 +13,30 @@ public class MovieCollection {
     }
 
     public void searchMovie(String titleSearch) {
+        titleSearch.toUpperCase();
         boolean found = false;
         for (Movie movie : MovieList) {
             if (movie.getTitle().equals(titleSearch)) {
                 System.out.println("The movie(s) found:" + movie.getTitle());
                 found = true;
-            } else {
-                System.out.println("Could not find the movie you are looking for");
             }
         }
+        if (!found){
+            System.out.println("Could not find the movie you are looking for");
+        }
+    }
 
-
+    public void searchMovieByLetter(String letterSearch) {
+        letterSearch.toUpperCase();
+        boolean found = false;
+        for (Movie movie : MovieList) {
+            if (movie.getTitle().contains(letterSearch)) {
+                System.out.println("Movies found: \n" + movie);
+                found = true;
+            }
+        }
+        if (!found){
+            System.out.println("Could not find any movies that include '" + letterSearch + "'");
+        }
     }
 }
