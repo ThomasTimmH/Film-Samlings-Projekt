d8e7f5b945f83fd9deb833854660cfd0900c9930
huma0001 <hussainomar5@gmail.com>
2024-10-24 15:22:05 +0200

    user story 5 + UI og lavet nogle ui introduktioner + velkomster

--- src/UI.java
+++ src/UI.java
@@ -1,2 +1,15 @@
 public class UI {
+
+    public void welcomeMsg(){
+        System.out.println("Welcome to your movie collection!");
+    }
+
+
+    public void introduktion(){
+        System.out.println("1. Create movie entry");
+        System.out.println("2. See movie list");
+        System.out.println("3. Search movie, by title");
+        System.out.println("4. Exit");
+    }
+
 }
