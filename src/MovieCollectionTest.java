477e152ea796f2db33eb573f3644c0a028c5a799
huma0001 <hussainomar5@gmail.com>
2024-10-25 11:32:58 +0200

    Added searchMovies/movie exists

--- src/MovieCollectionTest.java
+++ src/MovieCollectionTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ class MovieCollectionTest {
         Movie inception = new Movie("Inception", "gg", 2003, "Yes", 22, "Drama");
         gg.addMovie(inception);
         String expectedResult = "Inception";
-        String actualResult = "Inception";
+        String actualResult = inception.getTitle();
         assertEquals(expectedResult,actualResult);
     }
 
@@ -44,5 +45,54 @@ class MovieCollectionTest {
         ArrayList actualResult = movieCollection.getMovieList();
         assertEquals(actualResult.size(),3);
     }
+
+
+    @Test
+    void searchMovieExists(){
+        MovieCollection movieCollection = new MovieCollection();
+
+
+        // Act - Brug PlayList's findTrack metode til at finde dit nylavede Track objekt. Kald metoden getTitle på det track du har fundet og gem resultatet i en String variabel med navnet actualResult.
+        Movie movie3 = new Movie("Hunger Games", "gg", 2003, "Yes", 22, "Drama");
+        movieCollection.addMovie(movie3);
+        movieCollection.searchMovie("Hunger Games");
+        String actualResult = movie3.getTitle();
+        String expectedResult = "Hunger Games";
+
+        // Assert - Lav en String variabel som hedder expectedResult, og tildel den den String-værdi du leder efter.
+        // Brug herefter assertEquals således: assertEquals(expectedResult, actualResult).
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    void searchMovieDoesNotExist(){
+    }
+
+
+
+
+    @Test
+    void searchMoviesExists(){
+        MovieCollection movieCollection = new MovieCollection();
+
+
+        // Act - Brug PlayList's findTrack metode til at finde dit nylavede Track objekt. Kald metoden getTitle på det track du har fundet og gem resultatet i en String variabel med navnet actualResult.
+        Movie movie3 = new Movie("Hunger Games", "gg", 2003, "Yes", 22, "Drama");
+        Movie movie4 = new Movie("Harry Potter 3", "gg", 2003, "Yes", 22, "Drama");
+        movieCollection.addMovie(movie3);
+        movieCollection.addMovie(movie4);
+        movieCollection.searchMovie("H");
+        String actualResult = movieCollection.getMovieList().get(0).getTitle() + "" + movieCollection.getMovieList().get(1).getTitle();
+        String expectedResult = "Hunger Games" + "Harry Potter 3";
+
+        // Assert - Lav en String variabel som hedder expectedResult, og tildel den den String-værdi du leder efter.
+        // Brug herefter assertEquals således: assertEquals(expectedResult, actualResult).
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+
+
 }
 
