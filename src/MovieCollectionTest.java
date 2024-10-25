8afef0cd241961a3ed3e1c48a615ec78782d424a
ThomasTimmH <thha0006@stud.kea.dk>
2024-10-25 10:57:52 +0200

    Commit

--- src/MovieCollectionTest.java
+++ src/MovieCollectionTest.java
@@ -15,6 +15,19 @@ class MovieCollectionTest {
     }
 
     @Test
+    void addMovie1(){
+        MovieCollection movieCollection = new MovieCollection();
+        Movie movie1 = new Movie("Shawshank Redemption", "gg", 2003, "Yes", 22, "Drama");
+        Movie movie2 = new Movie("The Prestige", "gg", 2003, "Yes", 22, "Drama");
+        Movie movie3 = new Movie("Hunger Games", "gg", 2003, "Yes", 22, "Drama");
+        movieCollection.addMovie(movie1);
+        movieCollection.addMovie(movie2);
+        movieCollection.addMovie(movie3);
+
+    }
+
+
+    @Test
     void getMovieList() {
     }
 }
