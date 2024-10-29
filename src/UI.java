cfcf313ef5efdd907668b35a3fa6b1045e2cb3b6
huma0001 <hussainomar5@gmail.com>
2024-10-29 16:04:30 +0100

    added expection handling for some inputs in create/add movie

--- src/UI.java
+++ src/UI.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UI {
@@ -8,14 +9,7 @@ public class UI {
 
     public void Start() {
 
-
         System.out.println("Welcome to your movie collection!");
-        System.out.println();
-        System.out.println("1. Create movie entry");
-        System.out.println("2. See movie list");
-        System.out.println("3. Search movie, by title");
-        System.out.println("4. Edit movie by searching after title");
-        System.out.println("5. Exit");
 
         Movie testMovie = new Movie("Food", "Timm", 1993, "yes", 180, "Drama");
         Movie testMovie1 = new Movie("Penis", "Timm", 1992, "yes", 180, "Drama");
@@ -29,52 +23,54 @@ public class UI {
 
         boolean running = true;
         while (running) {
-            int userResponse = scan.nextInt();
-            switch (userResponse) {
-                case 1 -> {
-                    System.out.println("Enter Title:");
-                    scan.nextLine();
-                    String userTitle = scan.nextLine();
+            displayMenu();
+            int userResponse = validateInt();
+                switch (userResponse) {
+                    case 1 -> {
+                        System.out.println("Enter Title:");
+                        scan.nextLine();
+                        String userTitle = scan.nextLine();
 
-                    System.out.println("Enter a director");
-                    String userDirector = scan.nextLine();
+                        System.out.println("Enter a director");
+                        String userDirector = scan.nextLine();
 
-                    System.out.println("Enter the year the movie was made: ");
-                    int movieYear = scan.nextInt();
+                        System.out.println("Enter the year the movie was made: ");
+                        int movieYear = validateInt();
 
-                    System.out.println("Is the movie colored?");
-                    scan.nextLine();
-                    String userColor = scan.nextLine();
+                        System.out.println("Is the movie colored?");
+                        scan.nextLine();
+                        String userColor = scan.nextLine();
 
-                    System.out.println("Enter how long the movies is");
-                    int movieLength = scan.nextInt();
-
-
-                    System.out.println("Enter the movie's genre: ");
-                    scan.nextLine();
-                    String movieGenre = scan.nextLine();
+                        System.out.println("Enter how long the movies is in minutes");
+                        int movieLength = validateInt();
 
 
-                    Movie userMovie = new Movie(userTitle, userDirector, movieYear, userColor, movieLength, movieGenre);
+                        System.out.println("Enter the movie's genre: ");
+                        scan.nextLine();
+                        String movieGenre = scan.nextLine();
 
-                    controller.addMovie1(userMovie);
-                }
-                case 2 -> controller.showAllMovies();
-                case 3 -> {
-                    System.out.println("Please write the title of the movies you are looking for");
-                    scan.nextLine();
-                    String titleSearch = scan.nextLine();
-                    if (!titleSearch.isEmpty()) {
-                        controller.searchMovie1(titleSearch);
-                    } else {
-                        System.out.println("You have to write at least one letter to search");
+                        Movie userMovie = new Movie(userTitle, userDirector, movieYear, userColor, movieLength, movieGenre);
+
+                        controller.addMovie1(userMovie);
+                        System.out.println("Your movie " + userMovie.getTitle() + " has been added to the collection!");
                     }
-                }
-                case 4 ->  editMovie();
+                    case 2 -> controller.showAllMovies();
+                    case 3 -> {
+                        System.out.println("Please write the title of the movies you are looking for");
+                        scan.nextLine();
+                        String titleSearch = scan.nextLine();
+                        if (!titleSearch.isEmpty()) {
+                            controller.searchMovie1(titleSearch);
+                        } else {
+                            System.out.println("You have to write at least one letter to search");
+                        }
+                    }
+                    case 4 -> editMovie();
 
-                case 5 -> running = false;
-            }
+                    case 5 -> running = false;
+                }
         }
+
     }
 
     private void editMovie() {
@@ -96,8 +92,8 @@ public class UI {
         String newIsInColor = scan.nextLine();
 
         System.out.println("Enter new creation year: ");
-        int newYear = scan.nextInt();
         scan.nextLine(); // Håndter linjeskift
+        int newYear = scan.nextInt();
 
         System.out.println("Enter new genre: ");
         String newGenre = scan.nextLine();
@@ -108,6 +104,27 @@ public class UI {
             System.out.println("Movie updated successfully.");
         } else {
             System.out.println("Movie not found.");
+        }
+    }
+
+
+    private void displayMenu(){
+        System.out.println();
+        System.out.println("1. Create movie entry");
+        System.out.println("2. See movie list");
+        System.out.println("3. Search movie, by title");
+        System.out.println("4. Edit movie by searching after title");
+        System.out.println("5. Exit");
+    }
+
+    private int validateInt() {
+        while (true) {
+            try {
+                return scan.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid value.");
+                scan.nextLine(); // Consume the invalid input
+            }
         }
     }
 }
