d210fbe30a5075e311de8a5db1a5007b474de14c
MuzafferKaanCelik <muce0001@stud.kea.dk>
2024-10-31 13:09:00 +0100

    Dad

--- src/UI.java
+++ src/UI.java
@@ -128,7 +128,7 @@ public class UI {
         System.out.println("3. Search movie, by title");
         System.out.println("4. Edit movie by searching after title");
         System.out.println("5. Delete movie from list");
-        System.out.println("5. Exit");
+        System.out.println("6. Exit");
     }
 
     private int validateInt() {
